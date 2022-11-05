d067407dba2c14717be9053a7e3ec5fb32a1b26d
kumarafkbot <96719832+kumarafkbot@users.noreply.github.com>
2022-11-05 06:05:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClassification() 
+{
+     navigator.mediaDevices.getUserMedia({ audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/sNapaig01/model.json', modelReady);} 
+function modelReady()
+{
+     classifier.classify( gotResults); 
+}
+
+
+
